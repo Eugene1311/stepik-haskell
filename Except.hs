d6054d7ff339669912e9480a7b413d5ab7ee7ac4
evgenyfedoseev <ef7@mail.ru>
2024-02-17 14:48:27 +0300

    add trySum function

--- Except.hs
+++ Except.hs
@@ -27,10 +27,14 @@ tryIncrementEven x = if even x then return (x + 1) else throwE "Number is odd"
 data SumError = SumError Int ReadError
   deriving Show
 
-trySum :: [String] -> Except ReadError Integer
-trySum strings = except $ foldl foldFunc (Right 0) results
-  where results = fmap (runExcept . tryRead) strings -- [Either ReadError Integer]
-        resultsWithPositions = zip [1..] results
+trySum :: [String] -> Except SumError Integer
+trySum strings = except $ foldl foldFunc (Right 0) resultsWithSumError
+  where resultsWithReadError = fmap (runExcept . tryRead) strings -- [Either ReadError Integer]
+        resultsWithPositions = zipWith zipFunc [1..] resultsWithReadError -- [Either (Int, ReadError) Integer]
+        zipFunc i result = case result of
+                              Right x -> Right x
+                              Left ex -> Left (i ,ex)
+        resultsWithSumError = fmap (runExcept . (withExcept (\(i, readErr) -> SumError i readErr)) . except) resultsWithPositions -- [Except SumError Integer]
         foldFunc = \a -> \b -> ((Right (+)) <*> a <*> b)
-
--- withExcept :: (e -> e') -> Except e a -> Except e' a
+-- todo compare with it solution
+-- trySum xs = sum <$> traverse (\(i, s) -> withExcept (SumError i) $ tryRead s) (zip [1..] xs)
