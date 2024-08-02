7e74e51984ed10fba0e27a9373c35c34c9338535
evgenyfedoseev <ef7@mail.ru>
2024-08-03 02:03:49 +0300

    add SimpleError type

--- Except.hs
+++ Except.hs
@@ -38,3 +38,32 @@ trySum strings = except $ foldl foldFunc (Right 0) resultsWithSumError
         foldFunc = \a -> \b -> ((Right (+)) <*> a <*> b)
 -- todo compare with it solution
 -- trySum xs = sum <$> traverse (\(i, s) -> withExcept (SumError i) $ tryRead s) (zip [1..] xs)
+
+newtype SimpleError = Simple { getSimple :: String } 
+  deriving (Eq, Show)
+
+instance Semigroup SimpleError where
+  s1 <> s2 = Simple (getSimple s1 ++ getSimple s2)
+
+instance Monoid SimpleError where
+  mempty = Simple ""
+  mappend = (<>)
+
+lie2se :: ListIndexError -> SimpleError
+lie2se ErrNegativeIndex = Simple "[negative index]"
+lie2se (ErrIndexTooLarge i) = Simple ("[index (" ++ show i ++ ") is too large]")
+
+-- GHCi> toSimple = runExcept . withExcept lie2se
+-- GHCi> xs = [1,2,3]
+-- GHCi> toSimple $ xs !!! 42
+-- Left (Simple {getSimple = "[index (42) is too large]"})
+-- GHCi> toSimple $ xs !!! (-2)
+-- Left (Simple {getSimple = "[negative index]"})
+-- GHCi> toSimple $ xs !!! 2
+-- Right 3
+-- GHCi> import Data.Foldable (msum)
+-- GHCi> toSimpleFromList = runExcept . msum . map (withExcept lie2se)
+-- GHCi> toSimpleFromList [xs !!! (-2), xs !!! 42]
+-- Left (Simple {getSimple = "[negative index][index (42) is too large]"})
+-- GHCi> toSimpleFromList [xs !!! (-2), xs !!! 2]
+-- Right 3
