959220eef277d107f7e51daf097457398bcb1ad9
efedoseev <efedoseev@mts.ru>
2024-08-21 18:39:21 +0300

    extend PrsEP

--- PrsEP.hs
+++ PrsEP.hs
@@ -1,3 +1,5 @@
+import Control.Applicative
+
 newtype PrsEP a = PrsEP { runPrsEP :: Int -> String -> (Int, Either String (a, String)) }
 
 parseEP :: PrsEP a -> String -> Either String (a, String)
@@ -12,9 +14,9 @@ parseEP p  = snd . runPrsEP p 0
 -- (42,Left "pos 42: unexpected end of input")
 satisfyEP :: (Char -> Bool) -> PrsEP Char
 satisfyEP f = PrsEP func where
-    func position "" = (position + 1, Left  ("pos " ++ show (position + 1) ++ ": unexpected end of input"))
-    func position (y:ys) = (nextPosition, if (f y) then Right (y, ys) else Left  ("pos " ++ show nextPosition ++ ": unexpected " ++ [y])) where
-        nextPosition = position + 1
+  func position "" = (position + 1, Left  ("pos " ++ show (position + 1) ++ ": unexpected end of input"))
+  func position (y:ys) = (nextPosition, if (f y) then Right (y, ys) else Left  ("pos " ++ show nextPosition ++ ": unexpected " ++ [y])) where
+      nextPosition = position + 1
 
 -- GHCi> parseEP (charEP 'A') "ABC"
 -- Right ('A',"BC")
@@ -24,6 +26,25 @@ satisfyEP f = PrsEP func where
 -- Left "pos 1: unexpected end of input"
 charEP :: Char -> PrsEP Char
 charEP c = satisfyEP (== c)
--- charEP ch = PrsEP func where
---     func x "" = (x + 1, Left  ("pos " ++ show (x + 1) ++ ": unexpected end of input"))
---     func x (y:ys) = (x + 1, if (ch == y) then Right (y, ys) else Left  ("pos " ++ show (x + 1) ++ ": unexpected end of input"))
+
+instance Functor PrsEP where
+  fmap func prsEP = PrsEP $ \n -> \str -> fmap (fmap (\(a, s) -> (func a, s))) (runPrsEP prsEP n str)
+
+instance Applicative PrsEP where
+  pure a = PrsEP $ \n -> \str -> (n, Right (a, str))
+  pf <*> px = PrsEP $ \p -> \s -> case runPrsEP pf p s of
+    (p1, Left e) -> (p1, Left e)
+    (p1, Right (f, str1)) -> runPrsEP (f <$> px) p1 str1
+
+instance Alternative PrsEP where
+  empty = PrsEP $ \p -> \_ -> (p, Left "pos " ++ show p ++ ": empty alternative")
+  prsEP1 <|> prsEP2 = PrsEP $ \p -> \s -> case runPrsEP prsEP1 p s of
+    (p1, Right (x, str1)) -> (p1, Right (x, str1))
+    (p1, Left e1) -> case runPrsEP prsEP2 p s of
+      (p2, Right (x, str2)) -> (p2, Right (x, str2))
+      (p2, Left e2) -> if p2 > p1 then (p2, Left e2)
+        else (p1, Left e1)
+
+anyEP = satisfyEP (const True)
+testP = (,) <$> anyEP <* charEP 'B' <*> anyEP
+tripleP [a,b,c] = (\x y z -> [x,y,z]) <$> charEP a <*> charEP b <*>  charEP c
